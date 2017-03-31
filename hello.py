ef7b6711eb7e378dda2ca91a00b7bf573b266951
Miha Miklic <evilnull@gmail.com>
2017-03-31 17:40:22 +0200

    move sleep up

--- hello.py
+++ hello.py
@@ -1,12 +1,13 @@
 import time
+time.sleep(15)
 
 from flask import Flask
 application = Flask(__name__)
+
 
 @application.route("/")
 def hello():
 	return "Hello There!"
 
 if __name__ == "__main__":
-	time.sleep(15)
 	application.run(host='0.0.0.0')
